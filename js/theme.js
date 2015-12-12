78b746f2a59d0b9ec8f2d669e4fff63def707de4
Роман <Staremang@MacBook-Pro-Roman.local>
2015-12-13 05:48:42 +0600

    13-12-15-05-48

---
+++ js/theme.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function() {
+    $('.navbar-toggle').on("click", function() {
+        $('.navbar-toggle').toggleClass('navbar-toggle-visible');
+        $('.mobile-menu-wrapper').toggleClass('mobile-menu-wrapper--visible');
+})});
