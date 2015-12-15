e1d808789a1abd67be142a9a9556b7a80338e020
Роман <Staremang@MacBook-Pro-Roman.local>
2015-12-15 21:36:57 +0600

    Add testimonials

--- js/theme.js
+++ js/theme.js
@@ -4,3 +4,25 @@ $(document).ready(function() {
         $('.navbar-toggle').toggleClass('navbar-toggle-visible');
         $('.mobile-menu-wrapper').toggleClass('mobile-menu-wrapper--visible');
 })});
+
+window.addEventListener("scroll", reload);
+function reload()
+{
+    pos = bl.getBoundingClientRect().top + pageYOffset;
+clay();
+}
+document.addEventListener("scroll", clay);
+var bl = document.getElementById("clayBlock");
+
+var pos = bl.getBoundingClientRect().top + pageYOffset;
+clay();
+function clay() {
+    if(scrollY >= pos)
+    {
+        bl.classList.add("clay");
+    }
+    else
+    {
+        bl.classList.remove("clay");
+    }
+}
